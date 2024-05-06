5eaed5b936ab6f55a572f76fd2d98a4718b509e1
kwambiee <kwambokaj2.jk@gmail.com>
2024-05-06 16:20:56 +0300

    style the sign up, log in and forgot password page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,42 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        merienda: ["Merienda", "sans-serif"],
+      },
+      backgroundImage: {
+        circle: "url('/app/assets/images/highlighter.png')",
+      },
+      colors: {
+        scooter: {
+          50: "#f0fcfc",
+          100: "#e3fbfc",
+          200: "#baf2f7",
+          300: "#91e9f2",
+          400: "#46d7eb",
+          500: "#01c4e0",
+          600: "#00a5c9",
+          700: "#007ea8",
+          800: "#005f87",
+          900: "#004166",
+          950: "#002642",
+        },
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
