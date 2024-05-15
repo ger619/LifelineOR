254aab2c8ddf727d9ad785a6677b638d6441a1c5
ger619 <draker74@gmail.com>
2024-05-15 21:07:03 +0300

    added users admin to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ApplicationRecord
     Profile.create(user_id: id)
   end
   has_one :bio, foreign_key: :user_id, class_name: 'Bio', dependent: :destroy
-  has_many :consultation, foreign_key: :user_id, class_name: 'Consultation', dependent: :destroy
   has_one :profile, foreign_key: :user_id, class_name: 'Profile', dependent: :destroy
+  has_one :profile, foreign_key: :user_id, class_name: 'Profile', dependent: :destroy
+
+  has_many :exist, foreign_key: :user_id, class_name: 'Exist', dependent: :destroy
+  has_many :consultation, foreign_key: :user_id, class_name: 'Consultation', dependent: :destroy
 end
