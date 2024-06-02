ddc090d201683aeda144473fbf28addb79c617f4
ger619 <draker74@gmail.com>
2024-06-02 15:16:26 +0300

    adding validation on bio fine tuning

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -3,19 +3,20 @@ class Bio < ApplicationRecord
 
   # To show the age of a person
   def age
-    if date_of_birth.present?
-      now = Time.now.utc.to_date
-      now.year - date_of_birth.year - (date_of_birth.to_date.change(year: now.year) > now ? 1 : 0)
-    end
+    return unless date_of_birth.present?
+
+    now = Time.now.utc.to_date
+    now.year - date_of_birth.year - (date_of_birth.to_date.change(year: now.year) > now ? 1 : 0)
   end
 
-  validates :date_of_birth, presence: true
-  ##private
+  validates :date_of_birth, :language, :home_town, :city, :country, :about_me, :health_insurance, :nhif_number,
+            :nationality, :religion, :blood_group, :hospital_to_attend, presence: true
+  # #private
 
-  ##def date_of_birth_cannot_be_in_the_future
-    # code here
-    # if date_of_birth.present? && date_of_birth > Date.today
-    #   errors.add(:date_of_birth, "can't be in the future")
-    # end
-  #end
+  # #def date_of_birth_cannot_be_in_the_future
+  # code here
+  # if date_of_birth.present? && date_of_birth > Date.today
+  #   errors.add(:date_of_birth, "can't be in the future")
+  # end
+  # end
 end
