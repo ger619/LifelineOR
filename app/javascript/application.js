e7e65477113a20c9e26549ce82216aae17229e3c
ger619 <draker74@gmail.com>
2024-06-04 01:06:22 +0300

    add all

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import 'flowbite';
-import "flowbite/dist/flowbite.turbo.js";
-import "flowbite/dist/flowbite.css";
 //= require tinymce
